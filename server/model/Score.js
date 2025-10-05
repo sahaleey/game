90df207d9168113c08c47bc5ee9ab0a3b094bc71
crazydrace <ajua46244@gmail.com>
2025-10-05 15:22:51 +0530

    update

--- server/model/Score.js
+++ server/model/Score.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const ScoreSchema = new mongoose.Schema({
   name: { type: String, required: true },
   score: { type: Number, required: true },
-  plays: { type: Number, default: 0 },
+  plays: { type: Number },
   date: { type: Date, default: Date.now },
 });
 
